3e840f3193ed76cb4d9560d5d69582a2e698b05b
Simon Sprankel <simon@coderblog.de>
2013-01-04 22:34:51 +0100

    Wrap SKUs and product names correctly, fixes #19
    These values seem to work much better...

--- app/code/community/FireGento/Pdf/Model/Items/Default.php
+++ app/code/community/FireGento/Pdf/Model/Items/Default.php
@@ -53,14 +53,14 @@ class FireGento_Pdf_Model_Items_Default extends Mage_Sales_Model_Order_Pdf_Items
 
         // draw SKU
         $lines[0][] = array(
-            'text'  => Mage::helper('core/string')->str_split($this->getSku($item), 30),
+            'text'  => Mage::helper('core/string')->str_split($this->getSku($item), 17),
             'feed'  => $pdf->margin['left'] + 25,
             'font_size' => $fontSize
         );
 
         // draw Product name
         $lines[0][]= array(
-            'text' => Mage::helper('core/string')->str_split($item->getName(), 30, true, true),
+            'text' => Mage::helper('core/string')->str_split($item->getName(), 40, true, true),
             'feed' => $pdf->margin['left'] + 120,
             'font_size' => $fontSize
         );
