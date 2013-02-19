37f992d3b047507cfc85a7e794cadcf52901dc9e
Simon Sprankel <simonsprankel@gmail.com>
2013-02-19 13:11:12 +0100

    fixed exception with multiple pages, fixed position of notes

--- app/code/community/FireGento/Pdf/Model/Invoice.php
+++ app/code/community/FireGento/Pdf/Model/Invoice.php
@@ -144,7 +144,7 @@ class FireGento_Pdf_Model_Invoice extends FireGento_Pdf_Model_Abstract
             $page->drawLine($this->margin['left'], $this->y + 5, $this->margin['right'], $this->y + 5);
 
             /* add totals */
-            $this->insertTotals($page, $invoice);
+            $page = $this->insertTotals($page, $invoice);
 
             /* add note */
             if ($mode == 'invoice') {
@@ -253,7 +253,7 @@ class FireGento_Pdf_Model_Invoice extends FireGento_Pdf_Model_Abstract
 
         $this->pagecounter++;
         $this->y = 110;
-        $this->insertPageCounter($page);
+        $this->_insertPageCounter($page);
 
         $this->y = 800;
         $this->_setFontRegular($page, 9);
