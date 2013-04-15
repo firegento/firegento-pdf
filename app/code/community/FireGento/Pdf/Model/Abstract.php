6114ae85506f840629fae4e2c5b741531c009298
Thomas Birke <tbirke@netextreme.de>
2013-04-15 17:58:39 +0200

    init items array to show shipping tax even if creditmemo does not contain any item

--- app/code/community/FireGento/Pdf/Model/Abstract.php
+++ app/code/community/FireGento/Pdf/Model/Abstract.php
@@ -366,6 +366,7 @@ abstract class FireGento_Pdf_Model_Abstract extends Mage_Sales_Model_Order_Pdf_A
         }
 
         $groupedTax = array();
+        $items = array('items' => array());
 
         $items['items'] = array();
         foreach ($source->getAllItems() as $item) {
