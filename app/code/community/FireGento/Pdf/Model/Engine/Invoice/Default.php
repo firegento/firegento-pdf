fa2c7fcc17c4cb9dbe32bb861189feb17f66acdf
Paul Hachmang <paul@h-o.nl>
2013-09-19 16:10:07 +0200

    [+FEAT] Column-width of SKU wider and Qty narrower

--- app/code/community/FireGento/Pdf/Model/Engine/Invoice/Default.php
+++ app/code/community/FireGento/Pdf/Model/Engine/Invoice/Default.php
@@ -147,7 +147,7 @@ class FireGento_Pdf_Model_Engine_Invoice_Default extends FireGento_Pdf_Model_Eng
         $this->y -= 11;
         $page->drawText(Mage::helper('firegento_pdf')->__('Pos'), $this->margin['left'] + 3, $this->y, $this->encoding);
         $page->drawText(Mage::helper('firegento_pdf')->__('No.'), $this->margin['left'] + 25, $this->y, $this->encoding);
-        $page->drawText(Mage::helper('firegento_pdf')->__('Description'), $this->margin['left'] + 120, $this->y, $this->encoding);
+        $page->drawText(Mage::helper('firegento_pdf')->__('Description'), $this->margin['left'] + 130, $this->y, $this->encoding);
 
         $columns = array();
         $columns['price'] = array(
@@ -160,7 +160,7 @@ class FireGento_Pdf_Model_Engine_Invoice_Default extends FireGento_Pdf_Model_Eng
         );
         $columns['qty'] = array(
             'label'  => Mage::helper('firegento_pdf')->__('Qty'),
-            '_width' => 40
+            '_width' => 30
         );
         $columns['tax'] = array(
             'label'  => Mage::helper('firegento_pdf')->__('Tax'),
