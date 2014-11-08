56b861f1072c9ebb63c120a1e91c0a1d2f625e40
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2014-11-08 19:26:27 +0100

    FILE: ...ireGento/Pdf/controllers/Adminhtml/Sales/Order/CreditmemoController.php -------------------------------------------------------------------------------- FOUND 4 ERROR(S) AFFECTING 4 LINE(S) --------------------------------------------------------------------------------   2 | ERROR | Missing file doc comment   5 | ERROR | Missing class doc comment  24 | ERROR | Expected "} else {\n"; found "}\n        else {\n"  29 | ERROR | File must end with a newline character --------------------------------------------------------------------------------

--- src/app/code/community/FireGento/Pdf/controllers/Adminhtml/Sales/Order/CreditmemoController.php
+++ src/app/code/community/FireGento/Pdf/controllers/Adminhtml/Sales/Order/CreditmemoController.php
@@ -1,8 +1,34 @@
 <?php
-
+/**
+ * This file is part of a FireGento e.V. module.
+ *
+ * This FireGento e.V. module is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This script is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * PHP version 5
+ *
+ * @category  FireGento
+ * @package   FireGento_Pdf
+ * @author    FireGento Team <team@firegento.com>
+ * @copyright 2013 FireGento Team (http://www.firegento.com)
+ * @license   http://opensource.org/licenses/gpl-3.0 GNU General Public License, version 3 (GPLv3)
+ */
 require('Mage/Adminhtml/controllers/Sales/Order/CreditmemoController.php');
 
-class FireGento_Pdf_Adminhtml_Sales_Order_CreditmemoController extends Mage_Adminhtml_Sales_Order_CreditmemoController
+/**
+ * Class FireGento_Pdf_Adminhtml_Sales_Order_CreditmemoController
+ *
+ * @category FireGento
+ * @package  FireGento_Pdf
+ * @author   FireGento Team <team@firegento.com>
+ */
+class FireGento_Pdf_Adminhtml_Sales_Order_CreditmemoController
+    extends Mage_Adminhtml_Sales_Order_CreditmemoController
 {
 
     /**
@@ -13,10 +39,14 @@ class FireGento_Pdf_Adminhtml_Sales_Order_CreditmemoController extends Mage_Admi
         $this->_initCreditmemo();
         /** @see Mage_Adminhtml_Sales_Order_InvoiceController */
         if ($creditmemoId = $this->getRequest()->getParam('creditmemo_id')) {
-            if ($creditmemo = Mage::getModel('sales/order_creditmemo')->load($creditmemoId)) {
-                $pdf = Mage::getModel('sales/order_pdf_creditmemo')->getPdf(array($creditmemo));
+            if ($creditmemo = Mage::getModel('sales/order_creditmemo')
+                ->load($creditmemoId)
+            ) {
+                $pdf = Mage::getModel('sales/order_pdf_creditmemo')
+                    ->getPdf(array($creditmemo));
                 $this->_prepareDownloadResponse(
-                    Mage::helper('firegento_pdf')->getExportFilename('creditmemo', $creditmemo),
+                    Mage::helper('firegento_pdf')
+                        ->getExportFilename('creditmemo', $creditmemo),
                     $pdf->render(), 'application/pdf'
                 );
             }
